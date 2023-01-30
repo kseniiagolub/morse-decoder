9824e4dd184aa0e3370ff9cbfb39edd8eaee4ec7
Kseniia <94314480+kseniiagolub@users.noreply.github.com>
2023-01-30 11:22:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.slice(i, i + 10))
+    }
+    for (let i = 0; i < arr.length; i++) {
+        while (arr[i].includes('10')) {
+            arr[i] = arr[i].replace('10', '.')
+        }
+        while (arr[i].includes('11')) {
+            arr[i] = arr[i].replace('11', '-')
+        }
+        while (arr[i].includes('0')) {
+            arr[i] = arr[i].replace('0', '')
+        }
+    }
+    let res = ''
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < Object.keys(MORSE_TABLE).length; j++) {
+            if (arr[i] === Object.keys(MORSE_TABLE)[j]) {
+                res += Object.values(MORSE_TABLE)[j]
+            }
+        }
+        if (arr[i] === '**********') {
+            res += ' '
+        }
+    }
+    return res;
 }
 
 module.exports = {
